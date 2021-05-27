2515dbe0e567389678a5f234db84d71c274a0fa1
mohammed sadiq <ms0302500@gmail.com>
2021-05-27 15:54:12 +0530

    feat:

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Timer 
+A simple Stopwatch
+
  
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 window.onload=function (){
     var seconds=00;
     var tens=00;
+    var min=00;
+    var appendMin=document.getElementById("min");
     var appendTens=document.getElementById("tens");
     var appendSeconds=document.getElementById("seconds");
     var buttonStart=document.getElementById("start");
@@ -19,8 +21,10 @@ window.onload=function (){
         clearInterval(Interval);
         tens="00";
         seconds="00";
+        min="00"
         appendSeconds.innerHTML=tens;
         appendTens.innerHTML=seconds;
+        appendMin.innerHTML=min;
     }
     function timer(){
         tens++;
@@ -39,9 +43,19 @@ window.onload=function (){
         }
         if(seconds >9){
             appendSeconds.innerHTML=seconds;
+        } 
+        if(seconds > 59){
+            seconds="00"
+            appendSeconds.innerHTML=seconds;
+            min=min+1;
+            appendMin.innerHTML=min;
         }
-
-
+        if(min <= 9){
+            appendMin.innerHTML="0" + min;
+        }
+        if(min >9){
+            appendMin.innerHTML=min;
+        }
     }
 
     }
